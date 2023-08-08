726dd739e47071cfae55561d41cdd4e18a0843f8
dblay112 <brightdjogatse@gmail.com>
2023-08-08 14:26:20 +0000

    combination of 2 numbers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for value1 in range(0, 10):
+    for value2 in range(value1 + 1, 10):
+        if value1 == 8 and value2 == 9:
+            print("{}{}".format(value1, value2))
+        else:
+            print("{}{}".format(value1, value2), end=", ")
