eba85761ff9ea522bae24b62b81bfd4e6fee53f8
dblay112 <brightdjogatse@gmail.com>
2023-08-08 12:52:21 +0000

    modulo correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number % 10 > 5:
-    print("Last digit of {} is {} and is greater than 5.".format(number, number % 10))
-elif number % 10 == 0:
-    print("Last digit of {} is and is 0.".format(number, number % 10))
+last_digits = abs(number) % 10
+if number < 0:
+    last_digits = -last_digits
+print("Last digit of {} is {} and is ".format(number, last_digits), end="")
+if last_digits > 5:
+    print("greater than 5")
+elif last_digits == 0:
+    print("0")
 else:
-    print("Last digit of {} is and is less than 6 and not 0.".format(number, number % 10))
+    print("less than 6 and not 0")
