36a6725662d36ad6e4540d2a419e361548406d2a
dblay112 <brightdjogatse@gmail.com>
2023-08-11 00:56:58 -0700

    fixed errors

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
 
-    gem = dir(hidden_4)
-    for name in gem:
-        if gem[:2] != "__":
-            print(gem)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
