7cc41f085256db9aa1b2d1fea5912f8f134e94d3
dblay112 <brightdjogatse@gmail.com>
2023-08-12 03:00:25 -0700

    fixed errors

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
     
     for number in my_list:
-        print(f"{number:d}".format(my_list), end="\n")
+        print("{}".format(number), end="\n")
