00704dd0205c6b05ea33628abac6965a45f8e68f
dblay112 <brightdjogatse@gmail.com>
2023-08-16 15:01:12 -0700

    multiply by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+
+    new_dic = {}
+    for key, val in  a_dictionary.items():
+        new_val = val * 2
+        new_dic[key] = new_val
+
+    return new_dic
