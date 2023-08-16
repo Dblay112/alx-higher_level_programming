aec67d2659b5baf45fd0992b4f9e2eb1d4add6d5
dblay112 <brightdjogatse@gmail.com>
2023-08-16 15:04:14 -0700

    corrected pycodestye error

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,7 +3,7 @@
 def multiply_by_2(a_dictionary):
 
     new_dic = {}
-    for key, val in  a_dictionary.items():
+    for key, val in a_dictionary.items():
         new_val = val * 2
         new_dic[key] = new_val
 
