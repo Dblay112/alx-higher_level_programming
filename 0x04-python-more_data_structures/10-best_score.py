fadd2cd459eab43996d62ee62c669258997e6aec
dblay112 <brightdjogatse@gmail.com>
2023-08-16 16:49:08 -0700

    fixed errors

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,16 @@
 
 def best_score(a_dictionary):
 
+    if a_dictionary is None:
+        return None
+
     maxi = None
     maxi_1 = None
+    
     for key, value in a_dictionary.items():
         if maxi is None or value > maxi:
-        maxi = value
-        maxi_1 = key
+            maxi = value
+            maxi_1 = key
 
     return maxi_1
 
