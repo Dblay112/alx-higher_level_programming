6cb185e3ce699a1e839ea7dc0d12650850e5e050
dblay112 <brightdjogatse@gmail.com>
2023-08-28 15:38:56 -0700

    fixed output error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    result = None
     try:
         result = a / b
+        print("Inside result: {}".format(result))
     except ZeroDivisionError:
         return None
     finally:
