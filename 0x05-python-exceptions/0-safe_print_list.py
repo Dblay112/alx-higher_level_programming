4f9f0e45747120e78460d91654b16981d7cce5c4
dblay112 <brightdjogatse@gmail.com>
2023-08-28 08:09:19 -0700

    fixed errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             new_list += 1
-        except OutofRangeError:
+        except IndexError:
             break
     print("")
     return (new_list)
