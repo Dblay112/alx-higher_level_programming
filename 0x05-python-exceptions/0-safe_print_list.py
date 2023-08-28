2a23a80bd6a49c3760ec1e59f7af167507b6cc74
dblay112 <brightdjogatse@gmail.com>
2023-08-28 14:54:03 +0000

    minor fixes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,10 +5,11 @@ def safe_print_list(my_list=[], x=0):
     new_list = 0
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print("{}".format(my_list[i]), end=" ")
             new_list += 1
         print()
         return new_list
     except OutofRangeError:
-        print("not available")
-        return 0
+        break
+    print("")
+    return (new_list)
