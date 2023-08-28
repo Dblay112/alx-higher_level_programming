5d41855c9f4257ce5bbfea52ec9268187ab0eaad
dblay112 <brightdjogatse@gmail.com>
2023-08-28 15:10:38 -0700

    fixed pycodestyle errors

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,9 @@ def safe_print_list_integers(my_list=[], x=0):
     e_count = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]))
+            print("{}".format(my_list[i]), end="")
             e_count += 1
-        except TypeError:
-            pass
-    print()
+        except (ValueError, TypeError):
+            continue
+    print("")
     return e_count
