cd3778c0e778bf2ade00cf99d2136f1c913e0fe7
dblay112 <brightdjogatse@gmail.com>
2023-08-29 21:38:17 +0000

    fixed errors

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,10 +2,12 @@
 """1. Square with size """
 
 class Square:
-    """ a class Square that defines a square."""
-    def __init__(self, size=0):
-    """Initialize the new square class.
-    Args:
-    size: private instance attribute for my_square
-    """
-      self.__size = size
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
