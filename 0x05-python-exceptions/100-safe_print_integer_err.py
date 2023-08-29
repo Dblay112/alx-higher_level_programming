029eccbf13973b25ed44b9feaf7475b459f14d12
dblay112 <brightdjogatse@gmail.com>
2023-08-28 17:02:21 -0700

    final push

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
 
      try:
         print("{:d}".format(value))
-        return (True)
+        return True
     except (TypeError, ValueError):
-        print("Exception: {}", file=sys.stderr)
-        return (False)
+        print("Exception:", file=sys.stderr)
+        return False
