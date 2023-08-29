5de581eef322f5e5177ca3eddcfb3955c9a167ff
dblay112 <brightdjogatse@gmail.com>
2023-08-29 21:35:49 +0000

    1 square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""1. Square with size """
+
+class Square:
+    """ a class Square that defines a square."""
+    def __init__(self, size=0):
+    """Initialize the new square class.
+    Args:
+    size: private instance attribute for my_square
+    """
+      self.__size = size
