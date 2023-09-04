ad3ea81c4339446109a2f8580d66debdd2da9bc3
dblay112 <brightdjogatse@gmail.com>
2023-09-04 09:38:13 -0700

    change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """class that defines a rectangle"""
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -16,8 +17,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
-        print_symbol = "#"
+        Rectangle.number_of_instances += 1     
 
     @property
     def width(self):
