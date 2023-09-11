2691e6b98c7220e3db34d1fa71905567f7060af2
dblay112 <brightdjogatse@gmail.com>
2023-09-11 16:45:44 -0700

    inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class MyInt(int):
+    """class MyInt that inherits from int
+    """
+
+    def __eq__(self, other):
+        return (int(self) != other)
+
+    def __ne__(self, other):
+        return (int(self) == other)
