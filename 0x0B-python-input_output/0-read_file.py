71e3be0a543f33c6c432aea6efeb84e598083ef7
dblay112 <brightdjogatse@gmail.com>
2023-09-12 09:12:16 -0700

    fixed pycodestyle error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,6 @@
 
 def read_file(filename=""):
     """function that reads a text file and prints it to stdout"""
-    with open(filename ,encoding="utf-8") as e:
+
+    with open(filename, encoding="utf-8") as e:
         print(e.read(), end="")
