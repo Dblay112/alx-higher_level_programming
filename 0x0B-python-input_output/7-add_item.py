7ffc01e3f8edc6246e64a0aaf54c314da20460e5
dblay112 <brightdjogatse@gmail.com>
2023-09-12 10:25:44 -0700

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Load, add, save"""
+import json
+"""json module imported"""
+import sys
+"""argument module"""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    function that writes an Object to a text file
+    using a JSON representation
+    """
+    with open(filename, "w", encoding="utf-8") as e:
+        data = json.dumps(my_obj)
+        e.write(data)
+
+def load_from_json_file(filename):
+    """function that creates an Object from a “JSON file
+    """
+    with open(filename, "r", encoding="utf-8") as e:
+        return json.load(e)
+
+def main():
+    """main function to get arguments"""
+    arg_count = sys.argv[1:]
+    arg_list = []
+
+    for i in arg_count:
+        arg_list.append(i)
+
+    save_to_json_file(arg_list, "add_item.json")
