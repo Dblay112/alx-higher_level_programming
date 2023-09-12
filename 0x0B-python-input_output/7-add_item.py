0b450c11e3179283817f3a370e63a68d6a1e78c0
dblay112 <brightdjogatse@gmail.com>
2023-09-12 10:56:46 -0700

    fixed more errors

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,8 +4,8 @@ Load, add, save
 """
 from sys import argv
 from os import path
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def sum_items():
