910e264fdfd4a69313dbda9a2013b5c2a68375fa
dblay112 <brightdjogatse@gmail.com>
2023-09-12 09:03:29 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,8 +6,4 @@ def read_file(filename=""):
     """function that reads a text file and prints it to stdout"""
 
     with open('my_file_0.txt', 'r', encoding="utf-8") as e:
-        try:
-            for line in e:
-                print(line, end='')
-        finally:
-            e.close()
+        print(e.read(), end="")
