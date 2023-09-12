8dececc4ca053838ae62208aa267ae181dadb441
dblay112 <brightdjogatse@gmail.com>
2023-09-12 09:05:13 -0700

    fixed errors

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """function that reads a text file and prints it to stdout"""
-
-    with open('my_file_0.txt', 'r', encoding="utf-8") as e:
+    with open('my_file_0.txt', encoding="utf-8") as e:
         print(e.read(), end="")
