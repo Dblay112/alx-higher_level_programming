9d1f1129ea9b7ce3a8729fab712c7f746c51f4a0
dblay112 <brightdjogatse@gmail.com>
2023-09-12 08:52:08 -0700

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""0. Read file"""
+
+
+def read_file(filename=""):
+    """function that reads a text file and prints it to stdout"""
+
+    with open('my_file_0.txt', 'r', encoding="utf-8") as e:
+        print(e.read())
