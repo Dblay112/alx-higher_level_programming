833782f9ba849ab8aa3bf17feab7d69b9a7d82ec
dblay112 <brightdjogatse@gmail.com>
2023-09-16 03:34:41 -0700

    Almost a cirlce

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Base Class"""
+import json
+import turtle
 
 
 class Base:
@@ -18,3 +20,27 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        func that  returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        function that that writes the JSON string
+        representation of list_objs to a file:
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as me:
+            if list_objs is None:
+                me.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                me.write(Base.to_json_string(list_dicts))
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,3 +21,34 @@ class Square(Rectangle):
         """string representation of square"""
         s = "[Square] ({:d}) {:d}/{:d} - {:d}"
         return s.format(self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        if args is not None and len(args) > 0:
+            for i in args:
+                if i == 0:
+                    self.id = i
+                if i == 1:
+                    self.size = i
+                if i == 2:
+                    self.x = i
+                if i == 3:
+                    self.y = i
+        elif kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """return a dictionary"""
+        return {
+                'id' = self.id,
+                'size' = self.size,
+                'x' = self.x,
+                'y' = self.y
+                }
