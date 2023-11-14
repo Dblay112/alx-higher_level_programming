c893ae6d11a477ef5b13c2d0f169b0f70cfde30d
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2023-11-14 10:52:04 +0000

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const argNum = process.argv.length
+const argNum = process.argv.length;
 console.log(argNum === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
