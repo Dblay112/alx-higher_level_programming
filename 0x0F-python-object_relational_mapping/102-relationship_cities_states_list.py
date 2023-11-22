5abb0759283ca0144308b8231e48be187d186178
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2023-11-22 14:40:24 +0000

    Update 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""modules
-"""
+# Lists all City objects from the database hbtn_0e_101_usa.
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
