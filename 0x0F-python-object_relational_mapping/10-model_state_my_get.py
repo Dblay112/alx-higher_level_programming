b9221d5dc380e80ca7b481e986aacc4017bb7597
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2023-11-22 14:29:09 +0000

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """modules
 """
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    state_name = sys.argv[4]
-    Session = sessionmaker(bind=engine)    
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State)\
-                    .filter(State.name == state_name)\
-                    .order_by(State.id)
-    if (states is not None and states.count() > 0):
-        for state in states:
-            print('{}'.format(state.id))
-    else:
-        print('Not found')
+
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
+        print("Not found")
